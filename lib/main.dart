ececb1a9ab8b4473c397df06cbe15910375da750
AlexDinizJr <alexdinizjr13@gmail.com>
2025-04-01 00:23:32 -0300

    implementando grid e tornando os cards como botões

--- lib/main.dart
+++ lib/main.dart
@@ -25,10 +25,8 @@ class MyApp extends StatelessWidget {
 
 class ScaffoldBase extends StatelessWidget {
   final Widget body;
-  const ScaffoldBase({
-    super.key,
-    required this.body,
-  });
+
+  const ScaffoldBase({super.key, required this.body});
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +44,11 @@ class ScaffoldBase extends StatelessWidget {
             onPressed: () {
               Navigator.pushReplacement(
                 context,
-                MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Venda Móveis Online')),
+                MaterialPageRoute(
+                  builder:
+                      (context) =>
+                          const MyHomePage(title: 'Venda Móveis Online'),
+                ),
               );
             },
           ),
@@ -64,12 +66,13 @@ class ScaffoldBase extends StatelessWidget {
             },
           ),
           Builder(
-            builder: (context) => IconButton(
-              icon: const Icon(Icons.menu, color: Color(0xFFAF651F)),
-              onPressed: () {
-                Scaffold.of(context).openEndDrawer();
-              },
-            ),
+            builder:
+                (context) => IconButton(
+                  icon: const Icon(Icons.menu, color: Color(0xFFAF651F)),
+                  onPressed: () {
+                    Scaffold.of(context).openEndDrawer();
+                  },
+                ),
           ),
         ],
       ),
@@ -115,6 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
         padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
+          // Conteúdo do Body
           children: <Widget>[
             SizedBox(
               width: double.infinity,
@@ -135,82 +139,19 @@ class _MyHomePageState extends State<MyHomePage> {
                 backgroundColor: WidgetStateProperty.all(Colors.white),
               ),
             ),
-            // Card do Produto
-            Container(
-              margin: const EdgeInsets.only(top: 16),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(8),
-                      bottomLeft: Radius.circular(8),
-                    ),
-                    child: Image.asset(
-                      'assets/mesa-escritorio.png',
-                      width: 150,
-                      height: 150,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Mesa de Escritório',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Text(
-                        'Descrição',
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      const SizedBox(height: 15),
-                      Row(
-                        children: [
-                          const Text(
-                            'R\$ 1.000,00',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                          const SizedBox(width: 30),
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Color(0xFFAF651F),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: IconButton(
-                              icon: const Icon(
-                                Icons.add_shopping_cart,
-                                color: Color(0xFFF9E9DA),
-                              ),
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const PaginaProduto(),
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+            SizedBox(height: 10),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: double.infinity,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 2.8,
+                ),
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  return ProductCard(product: products[index]);
+                },
               ),
             ),
           ],
@@ -219,33 +160,171 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-class PaginaProduto extends StatelessWidget {
-  const PaginaProduto({super.key});
+
+//Classe Produto
+class Product {
+  final String name;
+  final String image;
+  final String description;
+  final double price;
+
+  Product({
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.description,
+  });
+}
+
+final List<Product> products = [
+  Product(
+    name: 'Mesa Escritório',
+    image: 'assets/mesa-escritorio.png',
+    price: 1000.00,
+    description: 'description',
+  ),
+  Product(
+    name: 'Cadeira Gamer',
+    image: 'assets/cadeira-gamer.png',
+    price: 850.00,
+    description: 'description',
+  ),
+  Product(
+    name: 'Sofá Confortável',
+    image: 'assets/sofa.png',
+    price: 2500.00,
+    description: 'description',
+  ),
+  Product(
+    name: 'Estante Moderna',
+    image: 'assets/estante.png',
+    price: 1200.00,
+    description: 'description',
+  ),
+];
+
+//Card do Produto
+class ProductCard extends StatelessWidget {
+  final Product product;
+
+  const ProductCard({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
-    return ScaffoldBase(
-        body: Title(
-          color: Colors.black,
-          child: Text(
-              'PaginaProduto'
+    // Inkwell torna o card como um botão para a página do produto
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder:
+                (context) => PaginaProduto(
+                  product: product,
+                ),
           ),
-        )
+        );
+      },
+      child: Container(
+        height: 150,
+        margin: const EdgeInsets.only(top: 16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(8),
+                bottomLeft: Radius.circular(8),
+              ),
+              child: Image.asset(
+                product.image,
+                width: 150,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(width: 8),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  product.name,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  product.description,
+                  style: const TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+                const SizedBox(height: 15),
+                Row(
+                  children: [
+                    Text(
+                      'R\$ ${product.price.toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(width: 30),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFAF651F),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.add_shopping_cart,
+                          color: Color(0xFFF9E9DA),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder:
+                                  (context) => PaginaProduto(product: product),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-class PaginaCarrinho extends StatelessWidget{
-  const PaginaCarrinho ({super.key});
+
+//Pagina do Produto
+class PaginaProduto extends StatelessWidget {
+  const PaginaProduto({super.key, required Product product});
 
   @override
   Widget build(BuildContext context) {
     return ScaffoldBase(
-      body: Title(
-          color: Colors.black,
-          child: Text(
-              'PaginaCarrinho'
-          ),
-      )
+      body: Title(color: Colors.black, child: Text('PaginaProduto')),
+    );
+  }
+}
+
+//Pagina do Carrinho
+class PaginaCarrinho extends StatelessWidget {
+  const PaginaCarrinho({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldBase(
+      body: Title(color: Colors.black, child: Text('PaginaCarrinho')),
     );
   }
 }
